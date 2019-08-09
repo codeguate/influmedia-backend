67a37b4ac340cf980021f5eb704eb809dfec2d31
Daniel Rodriguez <dannyjose1112@hotmail.com>
2019-08-09 15:53:35 -0600

    [UPDATED] added update for composer

--- app/Http/Controllers/NissanUsersController.php
+++ app/Http/Controllers/NissanUsersController.php
@@ -5,12 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\NissanUsers;
-use Google\Spreadsheet\DefaultServiceRequest;
-use Google\Spreadsheet\ServiceRequestFactory;
-use Google\Spreadsheet\SpreadsheetService;
+use Sheets;
 use DB;
-use Google_Client;
-use Google_Service_Drive;
 use Response;
 use Validator;
 class NissanUsersController extends Controller
@@ -79,6 +75,12 @@ class NissanUsersController extends Controller
     {
          {
             try {
+                // $rows = Sheets::spreadsheet('1CQ7noAv4qW95M918l_3cSFNHjHgqcBcDqsd_T0Qx7uw')->sheet('Nissan_Smartdsmedia')->insert(array(
+                //     "name" => "John",
+                //     "age" => 23,
+                //     "email" => "john@example.com"
+                // ));
+                // return $rows;
                 
                 DB::beginTransaction();
                 
@@ -87,72 +89,12 @@ class NissanUsersController extends Controller
                 $newObject->telefono            = $request->get('telefono');
                 $newObject->email            = $request->get('email');
                 $newObject->save();
-                $nombreSpreahSheet = "Nissan_Smartdsmedia";
-                // Nombre de hoja de cálculo
-                $hojaCalculo = "Registros";
-                // Aqui deben agregar el Path del archivo json.
-                $pathJson = "nissanform-678e0c4a47fc.json";
-                
-                // Inicializamos Google Client
-                $client = new Google_Client();
-                $client->setAuthConfig($pathJson);
-                $client->addScope(Google_Service_Drive::DRIVE);
-                
-                // si expiro el access token generamos otro
-                if($client->isAccessTokenExpired()) {
-                $client->fetchAccessTokenWithAssertion();
-                }
-                
-                // Obtenemos el access token
-                $obj_token = $client->getAccessToken();
-                $accessToken = $obj_token['access_token'];
-                
-                // Inicializamos google-spreadsheet-client
-                $serviceRequest = new DefaultServiceRequest($accessToken);
-                ServiceRequestFactory::setInstance($serviceRequest);
-                
-                //Obtenemos los Spreadsheets disponibles para las credenciales actuales
-                $spreadsheetService = new SpreadsheetService();
-                $spreadsheetFeed = $spreadsheetService->getSpreadsheetFeed();
-                
-                // Obtenemos la spreadsheet por su nombre
-                $spreadsheet = $spreadsheetFeed->getByTitle($nombreSpreahSheet);
-                
-                // Obtenemos las hojas de cálculo de la spreadsheet obetenida
-                $worksheetFeed = $spreadsheet->getWorksheets();
-                
-                // Obtenemos la hoja de cálculo por su nombre
-                $worksheet = $worksheetFeed->getByTitle($hojaCalculo);
-                $listFeed = $worksheet->getListFeed();
-                
-                /*
-                * Array de datos a agregar.
-                * Observar que el valor de la claves del array que representan los encabezados
-                * de las columnas van en minúsculas, en vez de Email sería email.
-                * Esto es porque los encabezados de columna deben coincidir exactamente
-                * con lo que fue devuelto por la API de Google y no por lo que se ve en Google Drive.
-                */
-                
-                $dataAgregar = array('nombre' => $newObject->nombres,
-                'email' => $newObject->email,
-                'telefono' => $newObject->telefono
-                );
-                
-                // Agregar datos
-                try{
-                    DB::commit();
-                    $listFeed->insert($dataAgregar);
-                    return Response::json($newObject, 200);
 
-                } catch (Exception $e) {
-                    $returnData = array (
-                        'status' => 500,
-                        'message' => $e->getMessage()
-                    );
                     
-                    DB::rollback();
-                    return Response::json($returnData, 500);
-                }
+                DB::commit();
+                  
+                return Response::json($newObject, 200);
+                
     
             } catch (Exception $e) {
                 $returnData = array (
